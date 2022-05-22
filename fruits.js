b5c5b6c7307a8b9970039435c75f9d496dac38a4
Shubhay S. Shah <69692903+TechnoXtremeDevZ@users.noreply.github.com>
2022-05-22 13:05:48 -0700

    Add files via upload

---
+++ fruits.js
@@ -0,0 +1,33 @@
+﻿var img = "";
+var status = "";
+
+function preload() {
+    img = loadImage("fruitbasket.jpg");
+}
+
+function setup() {
+    canvas = createCanvas(500, 300);
+    canvas.center();
+
+    objectDetection = ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function draw() {
+
+}
+
+function modelLoaded() {
+    console.log("Model is Loaded.");
+    status = true;
+    objectDetection.detect(img, gotResults);
+}
+
+function gotResults(error, results) {
+    if (error) {
+        console.error(error);
+    }
+    else {
+        console.log(results);
+    }
+}
