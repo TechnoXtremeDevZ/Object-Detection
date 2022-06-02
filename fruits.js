84cc4b322913e3e7797cfc10052d5fef7fe88ca2
Shubhay S. Shah <69692903+TechnoXtremeDevZ@users.noreply.github.com>
2022-06-01 18:29:36 -0700

    Update fruits.js

--- fruits.js
+++ fruits.js
@@ -1,12 +1,19 @@
-﻿var img = "";
+var img = "";
 var status = "";
+var objects = [];
+var percent = 0;
+var name = "";
+var x = 0;
+var y = 0;
+var width = 0;
+var height = 0;
 
 function preload() {
     img = loadImage("fruitbasket.jpg");
 }
 
 function setup() {
-    canvas = createCanvas(500, 300);
+    canvas = createCanvas(650, 450);
     canvas.center();
 
     objectDetection = ml5.objectDetector("cocossd", modelLoaded);
@@ -14,7 +21,25 @@ function setup() {
 }
 
 function draw() {
+    image(img, 0, 0, 650, 450);
 
+    if (status != "") {
+        for (var i = 0; i < objects.length; i++) {
+            percent = floor(objects[i].confidence * 100);
+            name = objects[i].label;
+            x = objects[i].x;
+            y = objects[i].y;
+            width = objects[i].width;
+            height = objects[i].height;
+
+            fill("red");
+            text(name + " " + percent + "%", x, y);
+            noFill();
+            stroke("red");
+            rect(x, y, width, height);
+            console.log("Showing Result Now.")
+        }
+    }
 }
 
 function modelLoaded() {
@@ -29,5 +54,7 @@ function gotResults(error, results) {
     }
     else {
         console.log(results);
+
+        objects = results;
     }
 }
